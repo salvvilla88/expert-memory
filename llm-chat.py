3d3e55b41980a59cafd737bf135a8aa8a9af9afd
Salvador <salv.villalobos8819@gmail.com>
2023-07-10 11:41:35 -0600

    Corrected variable name from model_name to model

--- llm-chat.py
+++ llm-chat.py
@@ -6,7 +6,7 @@ st.title('🦜🔗 Quickstart App')
 openai_api_key = st.secrets["OPENAI"]
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, model_name = 'gpt-4',openai_api_key=openai_api_key)
+  llm = OpenAI(temperature=0.7, model='gpt-4',openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
 with st.form('my_form'):
