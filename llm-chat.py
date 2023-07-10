e82c525440d23d483fb44c24796fb907fbcc1e3a
Salvador <salv.villalobos8819@gmail.com>
2023-07-10 16:58:28 -0600

    Added the use of SerpAPI

--- llm-chat.py
+++ llm-chat.py
@@ -1,16 +1,23 @@
 import streamlit as st
+
+from langchain import PromptTemplate
 from langchain.llms import OpenAI
+from langchain.agents import load_tools, initialize_agent
 
 st.title('🧙 Question Wiz')
 
 openai_api_key = st.secrets["OPENAI"]
+tool_names = ['serpapi']
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, model_name='gpt-3.5-turbo',openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+  llm = OpenAI(temperature=0, model_name='gpt-3.5-turbo',openai_api_key=openai_api_key)
+  tools = load_tools(tool_names,llm)
+  agent = initialize_agent(tools, llm, agent="zero-shot-react-description")
+  st.info(agent.run(input_text))
+  
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'Write your question here')
+  text = st.text_area('Prompt:', 'Write your question here')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
