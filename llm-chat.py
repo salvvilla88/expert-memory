e8ae7607625631687fa261236d245838c0446d14
salvvilla88 <salv.villalobos8819@gmail.com>
2023-08-17 10:11:35 -0600

    Removed redundant code

--- llm-chat.py
+++ llm-chat.py
@@ -9,17 +9,6 @@ from langchain.utilities import SerpAPIWrapper
 st.title('🧙 Question Wiz')
 
 openai_api_key = st.secrets['OPENAI']
-search = SerpAPIWrapper()
-tools = [
-    Tool(
-        name = "Search",
-        func=search.run,
-        description="useful for when you need to answer questions about current events or the current state of the world"
-    ),
-]
-llm = OpenAIChat(temperature=0, model_name='gpt-3.5-turbo',openai_api_key=openai_api_key)
-memory = ConversationBufferMemory(memory_key='chat_history')
-
 
 def generate_response(input_text):
   search = SerpAPIWrapper()
@@ -30,8 +19,6 @@ def generate_response(input_text):
           description="useful for when you need to answer questions about current events or the current state of the world"
       ),
   ]
-  llm = OpenAIChat(temperature=0, model_name='gpt-3.5-turbo',openai_api_key=openai_api_key)
-  memory = ConversationBufferMemory(memory_key='chat_history')
 
   prefix = """Have a conversation with a human, answering the following questions as best you can. You have access to the following tools:"""
   suffix = """Begin!"
@@ -46,14 +33,16 @@ def generate_response(input_text):
       suffix=suffix,
       input_variables=["input", "chat_history", "agent_scratchpad"]
   )
+  
+  memory = ConversationBufferMemory(memory_key='chat_history')
+  llm = OpenAIChat(temperature=0, model_name='gpt-3.5-turbo',openai_api_key=openai_api_key)
 
   llm_chain = LLMChain(llm=llm, prompt=prompt)
-  tool_names = [tool.name for tool in tools]
   agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
   agent_chain  = AgentExecutor.from_agent_and_tools(
     agent=agent, tools=tools, verbose=True, memory=memory
   )
-  st.info(agent_chain .run(input_text))
+  st.info(agent_chain.run(input_text))
   
 
 with st.form('my_form'):
